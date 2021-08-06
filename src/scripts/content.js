c53ded9d54d5f000964a91cc396ad09e8a2b760e
serglider <s-chernykh@outlook.com>
2021-08-06 21:22:22 +0300

    sample extension - wip 2

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,33 +2,33 @@ import browser from 'webextension-polyfill';
 import { restore } from './src/storage';
 
 let isSelected = false;
+let searchList;
 
 browser.runtime.onMessage.addListener((event) => {
     if (event === 'highlight') {
         if (isSelected) {
             unhighlight();
         } else {
-            highlight();
+            restore('searchList', []).then((list) => {
+                searchList = list;
+                highlight();
+            });
         }
-        isSelected = !!isSelected;
+        isSelected = !isSelected;
     }
 });
 
 function unhighlight() {
-    restore('searchList', []).then((searchList) => {
-        document.body.innerHTML = searchList.reduce((html, query) => {
-            const mark = `<mark>${query}</mark>`;
-            const re = new RegExp(mark, 'g');
-            return html.replace(re, query);
-        }, document.body.innerHTML);
-    });
+    document.body.innerHTML = searchList.reduce((html, query) => {
+        const mark = `<mark>${query}</mark>`;
+        const re = new RegExp(mark, 'g');
+        return html.replace(re, query);
+    }, document.body.innerHTML);
 }
 
 function highlight() {
-    restore('searchList', []).then((searchList) => {
-        document.body.innerHTML = searchList.reduce((html, query) => {
-            const re = new RegExp(query, 'g');
-            return html.replace(re, `<mark>${query}</mark>`);
-        }, document.body.innerHTML);
-    });
+    document.body.innerHTML = searchList.reduce((html, query) => {
+        const re = new RegExp(query, 'g');
+        return html.replace(re, `<mark>${query}</mark>`);
+    }, document.body.innerHTML);
 }
